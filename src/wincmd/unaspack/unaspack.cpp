a19e2cea6557c1e597646563ced5e9ce56a4601f
Alexander Vostres <alex.vostres@gmail.com>
2026-03-30 16:02:42 +0300

    Migrate unaspack to UNICODE

--- src/wincmd/unaspack/unaspack.cpp
+++ src/wincmd/unaspack/unaspack.cpp
@@ -1,8 +1,8 @@
 #include "stdafx.h"
 #include "unpack.h"
 
-#define PINAME "Aspack Unpacker PlugIn"
-#define PIABV "<APK>"
+#define PINAME _T("Aspack Unpacker PlugIn")
+#define PIABV _T("<APK>")
 
 #define MakePtr( cast, ptr, addValue ) (cast)( (ULONG_PTR)(ptr) + (ULONG_PTR)(addValue))
 
@@ -17,7 +17,7 @@ void __stdcall PexRegisterPlugIn(LPCTSTR *str)
 // __declspec(dllexport)
 void __stdcall PexAboutPlugIn(void)
 {
-	MessageBox(NULL, "Aspack Unpacker PlugIn\nVersion 0.1\n\nGANNIER F.", "UnAspack About", MB_ICONINFORMATION | MB_OK);
+	MessageBox(NULL, _T("Aspack Unpacker PlugIn\nVersion 0.1\n\nGANNIER F."), _T("UnAspack About"), MB_ICONINFORMATION | MB_OK);
 }
 
 LPVOID GetSectionPtr(PIMAGE_NT_HEADERS pNTHeader, ULONG_PTR imageBase)
@@ -37,7 +37,7 @@ BOOL __stdcall PexPreloadImage(PPGIParamsBlock pPGI)
 	DWORD id = pPGI->dwInterface;
 	PDWORD base = (DWORD *) pPGI->pInBuff;
 	
-	(pPGI->pCallBack)(id, 0, "Executing...");
+	(pPGI->pCallBack)(id, 0, _T("Executing..."));
 	
 	PIMAGE_DOS_HEADER pDosHdr = (PIMAGE_DOS_HEADER) base;
     PIMAGE_NT_HEADERS pSecondHdr = MakePtr( PIMAGE_NT_HEADERS, base, pDosHdr->e_lfanew );
@@ -45,34 +45,34 @@ BOOL __stdcall PexPreloadImage(PPGIParamsBlock pPGI)
 	DWORD max = (DWORD)((ULONG_PTR) pPGI->pInBuff + (ULONG_PTR) pPGI->dwInSize -(ULONG_PTR) NTbase);
 	if ( NTbase > ((PBYTE) base + pPGI->dwInSize)) 
 	{
-//		str.Format("%s: Error in EXE file", PIABV);
+//		str.Format(_T("%s: Error in EXE file", PIABV);
 //		(pPGI->pCallBack)(id, 0, str);  // 
 		return FALSE;
 	}
 	if (NTbase)
 	{ 
 		if (*(NTbase + 2) == 0xE9)
-			str.Format("%s: File compressed with Aspack ver 2.11", PIABV);
+			str.Format(_T("%s: File compressed with Aspack ver 2.11"), PIABV);
 		else if (max > 0x3B2)
 		{
 			if (*(PWORD)(NTbase + 0x3B2) == 0x01B8)
-				str.Format("%s: File compressed with Aspack ver 2.12/2.12a/2.12b", PIABV);
+				str.Format(_T("%s: File compressed with Aspack ver 2.12/2.12a/2.12b"), PIABV);
 			else if (*(PWORD)(NTbase + 0x3B3) == 0x01B8)
-				str.Format("%s: File compressed with Aspack ver ??", PIABV);
+				str.Format(_T("%s: File compressed with Aspack ver ??"), PIABV);
 			else if (max > 0x4F8) 
 			{
 				if (*(PWORD)(NTbase + 0x4F4) == 0x01B8)
-					str.Format("%s: File compressed with Aspack 2000", PIABV);
+					str.Format(_T("%s: File compressed with Aspack 2000"), PIABV);
 				else if (*(PWORD)(NTbase + 0x4F6) == 0x01B8)
-					str.Format("%s: File compressed with Aspack 2001", PIABV);
+					str.Format(_T("%s: File compressed with Aspack 2001"), PIABV);
 				else if (*(PWORD)(NTbase + 0x4F6) == 0x01B8 && *(NTbase + 0x4DF) == 0x000443A02)
-					str.Format("%s: File compressed with Aspack ver 2.1", PIABV);
+					str.Format(_T("%s: File compressed with Aspack ver 2.1"), PIABV);
 				else if (*(PWORD)(NTbase + 0x4F6) == 0x01B8 && *(NTbase + 0x4DF) == 0x0004439FD)
-					str.Format("%s: File compressed with Aspack ver ??", PIABV);
+					str.Format(_T("%s: File compressed with Aspack ver ??"), PIABV);
 				else if (*(PWORD)(NTbase + 0x3BA) == 0x7561)
-					str.Format("%s: File compressed with Aspack ver ??", PIABV);
+					str.Format(_T("%s: File compressed with Aspack ver ??"), PIABV);
 				else if (*(NTbase + 2) == 0xE8)
-					str.Format("%s: File compressed with Aspack ver 2.11c/d", PIABV);
+					str.Format(_T("%s: File compressed with Aspack ver 2.11c/d"), PIABV);
 				else return FALSE;
 			}	else return FALSE;
 		}	else return FALSE;

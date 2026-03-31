791df49d98ad51d4967bad0d64dc867778b7acf5
Alexander Vostres <alex.vostres@gmail.com>
2026-03-31 14:24:09 +0300

    Add support for modern Win10/11 version handling

--- src/common/verwin.cpp
+++ src/common/verwin.cpp
@@ -3,7 +3,7 @@
 #include "winbase.h"
 #include "verwin.h"
 
-/*   --- Changer dans Winbase.h --- 
+/*   --- Changer dans Winbase.h ---
 
 typedef struct _OSVERSIONINFOEXA {
     DWORD dwOSVersionInfoSize;
@@ -20,42 +20,144 @@ typedef struct _OSVERSIONINFOEXA {
 } OSVERSIONINFOEXA, *POSVERSIONINFOEXA, *LPOSVERSIONINFOEXA;
 */
 
+// RtlGetVersion doesn't lie about the OS version (unlike GetVersionEx on 8.1+)
+typedef LONG (WINAPI *pfnRtlGetVersion)(OSVERSIONINFOEXW *);
+
+static BOOL GetRealVersionInfo(OSVERSIONINFOEXW *osvi)
+{
+	pfnRtlGetVersion pRtlGetVersion = (pfnRtlGetVersion)
+		GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "RtlGetVersion");
+	if (pRtlGetVersion)
+	{
+		ZeroMemory(osvi, sizeof(OSVERSIONINFOEXW));
+		osvi->dwOSVersionInfoSize = sizeof(OSVERSIONINFOEXW);
+		if (pRtlGetVersion(osvi) == 0) // STATUS_SUCCESS
+			return TRUE;
+	}
+	return FALSE;
+}
+
 VERSION GetSystemVersion()
 {
 #define BUFSIZE 256
 	VERSION ver={0,0,0,0};
-   OSVERSIONINFOEX osvi;
-   BOOL bOsVersionInfoEx;
+   OSVERSIONINFOEXW osvi;
 
-   // Try calling GetVersionEx using the OSVERSIONINFOEX structure.
-   // If that fails, try using the OSVERSIONINFO structure.
-   ZeroMemory(&osvi, sizeof(OSVERSIONINFOEX));
-   osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
-
-   if( !(bOsVersionInfoEx = GetVersionEx ((OSVERSIONINFO *) &osvi)) )
+   // Use RtlGetVersion for accurate version on Windows 8.1+
+   // Falls back to GetVersionEx if RtlGetVersion is unavailable
+   if (!GetRealVersionInfo(&osvi))
    {
-      osvi.dwOSVersionInfoSize = sizeof (OSVERSIONINFO);
-      if (! GetVersionEx ( (OSVERSIONINFO *) &osvi) ) 
-         return ver;
+      OSVERSIONINFOEX osviLegacy;
+      ZeroMemory(&osviLegacy, sizeof(OSVERSIONINFOEX));
+      osviLegacy.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+      if (!GetVersionEx((OSVERSIONINFO *)&osviLegacy))
+      {
+         osviLegacy.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+         if (!GetVersionEx((OSVERSIONINFO *)&osviLegacy))
+            return ver;
+      }
+      // Copy to wide struct
+      ZeroMemory(&osvi, sizeof(osvi));
+      osvi.dwMajorVersion = osviLegacy.dwMajorVersion;
+      osvi.dwMinorVersion = osviLegacy.dwMinorVersion;
+      osvi.dwBuildNumber = osviLegacy.dwBuildNumber;
+      osvi.dwPlatformId = osviLegacy.dwPlatformId;
+      osvi.wServicePackMajor = osviLegacy.wServicePackMajor;
+      osvi.wServicePackMinor = osviLegacy.wServicePackMinor;
+      osvi.wSuiteMask = osviLegacy.wSuiteMask;
+      osvi.wProductType = osviLegacy.wProductType;
    }
-	
+
    ver.major = osvi.dwMajorVersion;
    ver.minor = osvi.dwMinorVersion;
    ver.sp = osvi.wServicePackMajor;
    ver.build = osvi.dwBuildNumber;
-//   ver.major = osvi.dwMajorVersion;
-//   ver.minor = osvi.dwMinorVersion;
 
 	switch (osvi.dwPlatformId)
    {// Test for the Windows NT product family.
       case VER_PLATFORM_WIN32_NT:
-		 if ( osvi.dwMajorVersion == 6)
+
+		 // Windows 10 / 11 / Server 2016+
+		 if ( osvi.dwMajorVersion == 10)
+		 {
+			if ( osvi.wProductType == VER_NT_WORKSTATION )
+			{
+				if ( osvi.dwBuildNumber >= 22000 )
+					ver.ver = WND_ELEVEN;
+				else
+					ver.ver = WND_TEN;
+			}
+			else
+			{
+				if ( osvi.dwBuildNumber >= 26100 )
+					ver.ver = WND_SERVER2025;
+				else if ( osvi.dwBuildNumber >= 20348 )
+					ver.ver = WND_SERVER2022;
+				else if ( osvi.dwBuildNumber >= 17763 )
+					ver.ver = WND_SERVER2019;
+				else
+					ver.ver = WND_SERVER2016;
+			}
+
+			// Modern edition detection via GetProductInfo
+			typedef BOOL (WINAPI *pfnGetProductInfo)(DWORD, DWORD, DWORD, DWORD, PDWORD);
+			pfnGetProductInfo pGetProductInfo = (pfnGetProductInfo)
+				GetProcAddress(GetModuleHandle(_T("kernel32.dll")), "GetProductInfo");
+			if (pGetProductInfo)
+			{
+				DWORD dwProductType = 0;
+				pGetProductInfo(osvi.dwMajorVersion, osvi.dwMinorVersion, 0, 0, &dwProductType);
+				switch (dwProductType)
+				{
+				case 0x00000065: // PRODUCT_CORE (Home)
+				case 0x00000063: // PRODUCT_CORE_SINGLELANGUAGE
+				case 0x00000062: // PRODUCT_CORE_N
+				case 0x00000064: // PRODUCT_CORE_COUNTRYSPECIFIC
+					ver.type = WND_HOME; break;
+				case 0x00000030: // PRODUCT_PROFESSIONAL
+				case 0x00000031: // PRODUCT_PROFESSIONAL_N
+					ver.type = WND_PRO; break;
+				case 0x000000A1: // PRODUCT_PRO_WORKSTATION
+				case 0x000000A2: // PRODUCT_PRO_WORKSTATION_N
+					ver.type = WND_PROWKS; break;
+				case 0x00000004: // PRODUCT_ENTERPRISE
+				case 0x0000001B: // PRODUCT_ENTERPRISE_N
+				case 0x00000046: // PRODUCT_ENTERPRISE_E
+					ver.type = WND_ENT; break;
+				case 0x00000079: // PRODUCT_EDUCATION
+				case 0x0000007A: // PRODUCT_EDUCATION_N
+					ver.type = WND_EDU; break;
+				case 0x00000007: // PRODUCT_STANDARD_SERVER
+				case 0x0000000D: // PRODUCT_STANDARD_SERVER_CORE
+					ver.type = WND_SE; break;
+				case 0x00000008: // PRODUCT_DATACENTER_SERVER
+				case 0x0000000C: // PRODUCT_DATACENTER_SERVER_CORE
+					ver.type = WND_DE; break;
+				}
+			}
+		 }
+		 // Windows 8.1 / Server 2012 R2
+		 else if ( osvi.dwMajorVersion == 6 && osvi.dwMinorVersion == 3 )
+		 {
+			if ( osvi.wProductType == VER_NT_WORKSTATION )
+				ver.ver = WND_81;
+			else ver.ver = WND_SERVER2012R2;
+		 }
+		 // Windows 8 / Server 2012
+		 else if ( osvi.dwMajorVersion == 6 && osvi.dwMinorVersion == 2 )
+		 {
+			if ( osvi.wProductType == VER_NT_WORKSTATION )
+				ver.ver = WND_8;
+			else ver.ver = WND_SERVER2012;
+		 }
+		 // Windows 7 / Vista / Server 2008
+		 else if ( osvi.dwMajorVersion == 6)
 		 {
 			 if ( osvi.dwMinorVersion == 0 )
 				if ( osvi.wProductType == VER_NT_WORKSTATION )
 					ver.ver = WND_VISTA;
 				else ver.ver = WND_SERVER2008;
-			if ( osvi.dwMinorVersion == 1 ) 
+			if ( osvi.dwMinorVersion == 1 )
 				if ( osvi.wProductType == VER_NT_WORKSTATION )
 					ver.ver = WND_SEVEN;
 				else ver.ver = WND_SERVER2008R2;
@@ -72,8 +174,8 @@ VERSION GetSystemVersion()
          if ( osvi.dwMajorVersion <= 4 )
 			 ver.ver = WND_NT;
 
-         // Test for specific product on Windows NT 4.0 SP6 and later.
-         if( bOsVersionInfoEx )
+         // Legacy edition detection via wSuiteMask (for pre-Win10)
+		 if ( osvi.dwMajorVersion < 10 )
          {
             // Test for the workstation type.
 			if ( osvi.wProductType == VER_NT_WORKSTATION )
@@ -108,7 +210,7 @@ VERSION GetSystemVersion()
 					else
 					  ver.type = WND_S;
 				}
-				else  // Windows NT 4.0 
+				else  // Windows NT 4.0
 				{
 					if( osvi.wSuiteMask & VER_SUITE_ENTERPRISE )
 						ver.type = WND_4EE;
@@ -117,40 +219,9 @@ VERSION GetSystemVersion()
 				}
 			}
 		 }
-// Test for specific product on Windows NT 4.0 SP5 and earlier
-		else  
-		{
-			HKEY hKey;
-			TCHAR szProductType[BUFSIZE];
-			DWORD dwBufLen=BUFSIZE * sizeof(TCHAR);
-			LONG lRet;
-			lRet = RegOpenKeyEx( HKEY_LOCAL_MACHINE,
-			   _T("SYSTEM\\CurrentControlSet\\Control\\ProductOptions"),
-			   0, KEY_QUERY_VALUE, &hKey );
-			if( lRet != ERROR_SUCCESS )
-			   return ver;
-
-			lRet = RegQueryValueEx( hKey, _T("ProductType"), NULL, NULL,
-			   (LPBYTE) szProductType, &dwBufLen);
-			if( (lRet != ERROR_SUCCESS) || (dwBufLen > BUFSIZE * sizeof(TCHAR)) )
-			   return ver;
-
-			RegCloseKey( hKey );
-
-			if ( lstrcmpi( _T("WINNT"), szProductType) == 0 )
-				if (osvi.dwMajorVersion == 4)
-					ver.type = WND_WKS4;
-				else ver.type = WND_WKS3;
-			if ( lstrcmpi( _T("LANMANNT"), szProductType) == 0 )
-				if (osvi.dwMajorVersion == 4)
-					ver.type = WND_4S;
-				else ver.type = WND_S;
-			if ( lstrcmpi( _T("SERVERNT"), szProductType) == 0 )
-					ver.type = WND_AS;
-		}
 // Display service pack (if any) and build number.
-        if( osvi.dwMajorVersion == 4 && 
-             lstrcmpi( osvi.szCSDVersion, _T("Service Pack 6") ) == 0 )
+        if( osvi.dwMajorVersion == 4 &&
+             lstrcmpi( osvi.szCSDVersion, L"Service Pack 6" ) == 0 )
         {
             HKEY hKey;
             LONG lRet;
@@ -176,16 +247,16 @@ VERSION GetSystemVersion()
          if (osvi.dwMajorVersion == 4 && osvi.dwMinorVersion == 0)
          {
 			ver.ver = WND_95;
-            if ( osvi.szCSDVersion[1] == 'C' || osvi.szCSDVersion[1] == 'B' )
+            if ( osvi.szCSDVersion[1] == L'C' || osvi.szCSDVersion[1] == L'B' )
 			      ver.type = WND_OSR2;
-         } 
+         }
 
          if (osvi.dwMajorVersion == 4 && osvi.dwMinorVersion == 10)
          {
 			ver.ver = WND_98;
-             if ( osvi.szCSDVersion[1] == 'A' ) 
+             if ( osvi.szCSDVersion[1] == L'A' )
 				 ver.type = WND_OSR1;
-         } 
+         }
 
          if (osvi.dwMajorVersion == 4 && osvi.dwMinorVersion == 90)
 			 ver.type = WND_ME;
@@ -194,7 +265,7 @@ VERSION GetSystemVersion()
       case VER_PLATFORM_WIN32s: ver.ver = WND_32S;
          break;
    }
-   return ver; 
+   return ver;
 }
 
 typedef void (WINAPI *PGNSI)(LPSYSTEM_INFO);
@@ -210,12 +281,22 @@ CString GetSystemVersionName(VERSION &ver)
 	switch (ver.ver)
 	{
 		case 0 : break;
-		case WND_SERVER2008R2 : str = _T("Microsoft Windows Server 2008 Revision 2 family "); break;
-		case WND_SERVER2008 : str = _T("Microsoft Windows Server 2008 family "); break;
-		case WND_SEVEN : str = _T("Microsoft Windows Seven "); break;
+		case WND_SERVER2025 : str = _T("Microsoft Windows Server 2025 "); break;
+		case WND_SERVER2022 : str = _T("Microsoft Windows Server 2022 "); break;
+		case WND_SERVER2019 : str = _T("Microsoft Windows Server 2019 "); break;
+		case WND_SERVER2016 : str = _T("Microsoft Windows Server 2016 "); break;
+		case WND_ELEVEN : str = _T("Microsoft Windows 11 "); break;
+		case WND_TEN : str = _T("Microsoft Windows 10 "); break;
+		case WND_SERVER2012R2 : str = _T("Microsoft Windows Server 2012 R2 "); break;
+		case WND_SERVER2012 : str = _T("Microsoft Windows Server 2012 "); break;
+		case WND_SERVER2008R2 : str = _T("Microsoft Windows Server 2008 R2 "); break;
+		case WND_SERVER2008 : str = _T("Microsoft Windows Server 2008 "); break;
+		case WND_SEVEN : str = _T("Microsoft Windows 7 "); break;
+		case WND_81 : str = _T("Microsoft Windows 8.1 "); break;
+		case WND_8 : str = _T("Microsoft Windows 8 "); break;
 
 		case WND_VISTA: str = _T("Microsoft Windows Vista "); break;
-		case WND_SERVER2003: str = _T("Microsoft Windows Server 2003 family "); break;
+		case WND_SERVER2003: str = _T("Microsoft Windows Server 2003 "); break;
 		case WND_XP: str = _T("Microsoft Windows XP ");break;
 		case WND_2K: str = _T("Microsoft Windows 2000 ");break;
 		case WND_NT: str = _T("Microsoft Windows NT ");break;
@@ -224,15 +305,31 @@ CString GetSystemVersionName(VERSION &ver)
 		case WND_ME: str = _T("Microsoft Windows Millennium Edition\n");break;
 		case WND_32S: str = _T("Microsoft Win32s\n");break;
 	}
+#ifndef PROCESSOR_ARCHITECTURE_ARM
+#define PROCESSOR_ARCHITECTURE_ARM 5
+#endif
+#ifndef PROCESSOR_ARCHITECTURE_ARM64
+#define PROCESSOR_ARCHITECTURE_ARM64 12
+#endif
 	if ( si.wProcessorArchitecture==PROCESSOR_ARCHITECTURE_IA64 )
 		str += _T("Itanium-based Systems ");
 	else if ( si.wProcessorArchitecture==PROCESSOR_ARCHITECTURE_AMD64 )
 		str += _T("x64 ");
+	else if ( si.wProcessorArchitecture==PROCESSOR_ARCHITECTURE_ARM64 )
+		str += _T("ARM64 ");
+	else if ( si.wProcessorArchitecture==PROCESSOR_ARCHITECTURE_ARM )
+		str += _T("ARM ");
+
 	switch (ver.type)
 	{
 		case WND_WKS4: str += _T("Workstation 4.0 "); break;
 		case WND_HE:  str += _T("Home Edition ");break;
 		case WND_PE: str += _T("Professional Edition ");break;
+		case WND_PRO: str += _T("Pro ");break;
+		case WND_PROWKS: str += _T("Pro for Workstations ");break;
+		case WND_HOME: str += _T("Home ");break;
+		case WND_ENT: str += _T("Enterprise ");break;
+		case WND_EDU: str += _T("Education ");break;
 		case WND_DE: str += _T("Datacenter Edition ");break;
 		case WND_EE: str += _T("Enterprise Edition ");break;
 		case WND_WE: str += _T("Web Edition ");break;
@@ -254,6 +351,39 @@ CString strtp;
 			strtp.Format( _T("Service Pack 6a (Build %d)\n"), ver.build );
 		else strtp.Format( _T("Service Pack %d (Build %d)\n"), ver.sp, ver.build );
 	}
+	else if ( ver.major >= 10 )
+	{
+		// Read DisplayVersion (e.g. "25H2") and UBR (e.g. 8106) from registry
+		CString szDisplayVer;
+		DWORD dwUBR = 0;
+		{
+			CRegKey key;
+			if ( key.Open(HKEY_LOCAL_MACHINE, _T("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion"), KEY_READ) == ERROR_SUCCESS )
+			{
+				TCHAR szVal[64] = {0};
+				DWORD dwCount = 64;
+				if ( key.QueryValue(szVal, _T("DisplayVersion"), &dwCount) == ERROR_SUCCESS && szVal[0] )
+					szDisplayVer = szVal;
+				DWORD dwSize = sizeof(DWORD);
+				RegQueryValueEx(key, _T("UBR"), NULL, NULL, (LPBYTE)&dwUBR, &dwSize);
+				key.Close();
+			}
+		}
+		if ( !szDisplayVer.IsEmpty() )
+		{
+			if (dwUBR > 0)
+				strtp.Format( _T("Version %s (build %d.%d)"), (LPCTSTR)szDisplayVer, ver.build, dwUBR );
+			else
+				strtp.Format( _T("Version %s (build %d)"), (LPCTSTR)szDisplayVer, ver.build );
+		}
+		else
+		{
+			if (dwUBR > 0)
+				strtp.Format( _T("Version %d.%d (build %d.%d)"), ver.major, ver.minor, ver.build, dwUBR );
+			else
+				strtp.Format( _T("Version %d.%d (build %d)"), ver.major, ver.minor, ver.build );
+		}
+	}
 	else {
 		if (ver.sp > 0)
 			strtp.Format( _T("Version %d.%d (build %d) Service pack %d \n"), ver.major, ver.minor, ver.build, ver.sp);
@@ -262,5 +392,5 @@ CString strtp;
 
 	}
 	str += strtp;
-   return str; 
+   return str;
 }
--- src/common/verwin.h
+++ src/common/verwin.h
@@ -3,6 +3,14 @@
 
 
 // version
+#define WND_SERVER2025 21
+#define WND_SERVER2022 20
+#define WND_SERVER2019 19
+#define WND_SERVER2016 18
+#define WND_ELEVEN 17
+#define WND_TEN    16
+#define WND_SERVER2012R2 15
+#define WND_SERVER2012 14
 #define WND_SERVER2008R2 13
 #define WND_SERVER2008 12
 #define WND_SEVEN 11
@@ -16,8 +24,10 @@
 #define WND_98   3
 #define WND_95   2
 #define WND_32S  1
+#define WND_81   22
+#define WND_8    23
 
-// type 
+// type
 #define WND_WKS3 1
 #define WND_WKS4 2
 #define WND_S    3
@@ -31,29 +41,34 @@
 #define WND_WE	11
 #define WND_SE	12
 #define WND_DS	13
+#define WND_PRO  14
+#define WND_HOME 15
+#define WND_EDU  16
+#define WND_ENT  17
+#define WND_PROWKS 18
 
 #define WND_OSR2 102
 #define WND_OSR1 101
 /**/
 	// #define VER_NT_SERVER 0x80000000
-#ifndef VER_NT_WORKSTATION 
+#ifndef VER_NT_WORKSTATION
 	#define VER_NT_WORKSTATION 0x1
 	#define VER_NT_DOMAIN_CONTROLLER 0x2
 	#define VER_NT_SERVER 0x3
 	#define VER_WORKSTATION_NT 0x40000000
 	 // Microsoft Small Business Server
-	#define VER_SUITE_SMALLBUSINESS 0x1    
+	#define VER_SUITE_SMALLBUSINESS 0x1
 	 // Win2k Adv Server or .Net Enterprise Server
-	#define VER_SUITE_ENTERPRISE 0x2       
-	 // Terminal Services is installed.  
-	#define VER_SUITE_TERMINAL 0x10        
+	#define VER_SUITE_ENTERPRISE 0x2
+	 // Terminal Services is installed.
+	#define VER_SUITE_TERMINAL 0x10
 		// Win2k Datacenter
 	#define VER_SUITE_DATACENTER 0x80
 	 // Terminal server in remote admin mode
 	#define VER_SUITE_SINGLEUSERTS 0x100
 	#define VER_SUITE_PERSONAL 0x200
 	 // Microsoft .Net webserver installed
-	#define VER_SUITE_BLADE 0x400     
+	#define VER_SUITE_BLADE 0x400
 #endif
 /**/
 
